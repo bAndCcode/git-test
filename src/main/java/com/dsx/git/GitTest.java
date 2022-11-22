cde51ef9a0a1212e63b1a58042184cbe6be2b44c
Dsx <Dsx@0407.com>
2022-11-22 11:34:31 +0800

    first commit

---
+++ src/main/java/com/dsx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.dsx.git;
+
+/**
+ * @author 丁硕轩
+ * @create 2022-11-22-11:31
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
